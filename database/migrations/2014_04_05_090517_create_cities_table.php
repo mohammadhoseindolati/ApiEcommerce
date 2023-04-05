fa6fcc3f3aeccce1b8d7599b1f350242822f603a
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2023-04-05 15:20:16 +0430

    add migration

--- database/migrations/2014_04_05_090517_create_cities_table.php
+++ database/migrations/2014_04_05_090517_create_cities_table.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->foreignId('province_id');
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('cascade');
+
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cities');
     }
 };
